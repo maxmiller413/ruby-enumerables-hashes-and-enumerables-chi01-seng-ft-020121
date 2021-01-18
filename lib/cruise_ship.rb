93dc6c20a1a8b4fd399cff004e8ece161a0a1b5e
Max Miller <maxmiller413@gmail.com>
2021-01-18 14:06:50 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,13 +7,12 @@
 # }
 
 def select_winner(passengers)
-  winner = ""
+winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
- 
-winner
+    winner = name 
+  end 
+end 
+winner 
 end 
   # add the code snippet here!
