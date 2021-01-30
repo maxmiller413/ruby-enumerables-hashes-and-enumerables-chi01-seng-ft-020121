6f3af934b0549c8e3932dda17ec62715643518f6
Max Miller <maxmiller413@gmail.com>
2021-01-30 03:57:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,8 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  birthday_kids.each do |kid, age|
+  puts "Happy Birthday #{kid}! You are now #{age} years old!"
   end 
   # add your code snippet here!
 end
