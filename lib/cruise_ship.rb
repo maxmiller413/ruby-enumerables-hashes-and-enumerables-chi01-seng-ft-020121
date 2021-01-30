822c502c4a7f66d6f9676d5a7bbcc456feff2a6f
Max Miller <maxmiller413@gmail.com>
2021-01-30 03:51:40 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,7 +7,13 @@
 # }
 
 def select_winner(passengers)
- 
+  winner = "" 
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start?("A")
+      winner = name 
+    end 
+  end 
+  winner 
 end 
 
   # add the code snippet here!
